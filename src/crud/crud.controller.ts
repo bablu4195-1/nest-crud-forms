6b8b4ee58a75ecee7cf1df5297a6024c1e47e76f
Venkata Sai Teja Mukkapati <55186465+bablu4195-1@users.noreply.github.com>
2024-04-05 20:40:53 +0530

    protected auth

--- src/crud/crud.controller.ts
+++ src/crud/crud.controller.ts
@@ -5,11 +5,11 @@ import { UpdateCrudDto } from './dto/update-crud.dto';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('crud')
+@UseGuards(AuthGuard('jwt'))
 export class CrudController {
   constructor(private readonly crudService: CrudService,) {}
 
   @Post()
-  @UseGuards(AuthGuard('jwt'))
   create(@Body() createCrudDto: CreateCrudDto) {
     console.log("called and save",createCrudDto);
     return this.crudService.create(createCrudDto);
@@ -21,20 +21,19 @@ export class CrudController {
     return this.crudService.findAll();
   }
 
+
   @Get(':id')
-  @UseGuards(AuthGuard('jwt'))
   findOne(@Param('id') id: string) {
     return this.crudService.findOne(id);
   }
 
   @Patch(':id')
-  @UseGuards(AuthGuard('jwt'))
   update(@Param('id') id: string, @Body() updateCrudDto: UpdateCrudDto) {
     return this.crudService.update(id, updateCrudDto);
   }
 
+ 
   @Delete(':id')
-  @UseGuards(AuthGuard('jwt'))
   remove(@Param('id') id: string) {
     return this.crudService.remove(id);
   }
--- src/crud/crud.module.ts
+++ src/crud/crud.module.ts
@@ -4,12 +4,13 @@ import { CrudController } from './crud.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Crud,CrudSchema } from './crud.schema';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
 
 @Module({
   imports:[MongooseModule.forFeature([{ name: Crud.name, schema: CrudSchema }]),
   PassportModule
 ],
   controllers: [CrudController],
-  providers: [CrudService],
+  providers: [CrudService, JwtStrategy],
 })
 export class CrudModule {}
